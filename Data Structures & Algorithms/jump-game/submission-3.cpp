4dbb70b1a8ac33f4f2a64a8ee9ed75fb2c8f82d1
Sumukh Venkatesh <60518194+SilverCobra-prog@users.noreply.github.com>
2026-07-20 23:16:59 -0700

    Add: jump-game - submission-3

---
+++ Data Structures & Algorithms/jump-game/submission-3.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int n = nums.size();
+        vector<bool> dp(n);
+        dp[0] = true;
+        for(int i = 1; i < n; i++) {
+            for(int j = i-1; j >= 0; j--) {
+                if(j + nums[j] >= i) {
+                    dp[i] = dp[i] || dp[j];
+                }
+            }
+        }
+        return dp[n-1];
+    }
+};
